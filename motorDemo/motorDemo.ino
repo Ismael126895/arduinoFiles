f268767d8e3dc76a61a68f934288f03d81b1726b
Lennox <Lenojore67@gmail.com>
2023-11-15 17:53:08 +0300

    added a potentiometer to control motor

--- motorDemo/motorDemo.ino
+++ motorDemo/motorDemo.ino
@@ -1,16 +1,27 @@
-int pin7=7;
-int pin8 =8;
-int pin11 =11;
+int pin7=7;//turn the rotor clockwise
+int pin8 =8;//turning the rotor anticlockwise
+int pin11 =11;//adjust the speed of the motor
+int potVal = 0;//collects potentiometer values
+int mSpeed = 0;//converts potentiometer values to speed
 void setup(){
   Serial.begin(9600);
   pinMode(pin7,OUTPUT);
   pinMode(pin8,OUTPUT);
- 
+  Serial.begin(9600);
   
 }
 void loop(){
   
-   digitalWrite(pin7,HIGH);
-  digitalWrite(pin8,LOW);
-  analogWrite(pin11,255);
+  potVal=analogRead(A5);
+  Serial.print("pot value: ");
+  Serial.println(potVal);
+  
+  mSpeed=(255./1023.)*potVal;
+  
+  Serial.print("Motor Speed: ");
+  Serial.println(mSpeed);
+  digitalWrite(pin7,LOW);
+  digitalWrite(pin8,HIGH);
+  analogWrite(pin11,mSpeed);
+  delay(500);
 }
